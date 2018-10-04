c8d0670f346b1becde392ba8fe604983d094b412
hsampso2 <35956862+hsampso2@users.noreply.github.com>
2018-10-04 10:46:01 -0500

    Add files via upload
    one function works but now i have to do the rest :')

--- Disassembler.py
+++ Disassembler.py
@@ -25,31 +25,32 @@ output_file = open("MIPS_asm.txt", "w")
 
 for line in input_file:
     line = line.replace("\n", "")  # remove 'endline' character
-    if(line[0] == '0'):
+    if(line[0:1] == '0'):
         line = line.replace("0", "", 1)  # remove parity bit
     else:
         line = line.replace("1", "", 1)  # remove parity bit
     line = line.replace(" ", "")  # remove spaces anywhere in line
 
-    if (line[1:3] == '000'):  # lw
-        line[1:3] = line[1:3].replace('000', 'lw ')  # remove 000 and use lw
-        if(line[4:5] == '00'):
-            line[4:5] = line[4:5].replace('00', '$0, ')
-        elif(line[4:5] == '01'):
-            line[4:5] = line[4:5].replace('01', '$1, ')
-        elif(line[4:5] == '10'):
-            line[4:5] = line[4:5].replace('10', '$2, ')
+    if (line[0:3] == '000'):  # lw
+        line = line.replace("000", "lw ", 1)  # remove 000 and use lw
+        if(line[3:5] == '00'):
+            line = line.replace('00', '$0, ', 1)
+        elif(line[3:5] == '01'):
+            line = line.replace('01', '$1, ', 1)
+        elif(line[3:5] == '10'):
+            line = line.replace('10', '$2, ', 1)
         else:
-            line[4:5] = line[4:5].replace('11', '$3, ')
+            line = line.replace('11', '$3, ', 1)
 
-        if (line[6:7] == '00'):
-            line[6:7] = line[6:7].replace('00', '$0')
-        elif (line[6:7] == '01'):
-            line[6:7] = line[6:7].replace('01', '$1')
-        elif (line[6:7] == '10'):
-            line[6:7] = line[6:7].replace('10', '$2')
+        if (line[7:9] == '00'):
+            line = line.replace('00', '$0')
+        elif (line[7:9] == '01'):
+            line = line.replace('01', '$1')
+        elif (line[7:9] == '10'):
+            line = line.replace('10', '$2')
         else:
-            line[6:7] = line[6:7].replace('11', '$3')
+            line = line.replace('11', '$3')
+
 
     elif (line[1:3] == '001'):  # sw
         line[1:3] = line[1:3].replace('001', 'sw ')
@@ -220,6 +221,8 @@ for line in input_file:
         line[1:7] = line[1:7].replace('1111111', 'Halt')
     else:
         print("Unknown instruction:" + line)
+
+    output_file.write(line)
 
 input_file.close()
 output_file.close()
